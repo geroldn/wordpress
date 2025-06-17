96c717b371e072efdacb6d86e8954dc259872621
geroldn <gerold@nicolasen.com>
2025-06-17 22:15:07 +0200

    wp-config.php not in temp

--- pl_entrypoint.sh
+++ pl_entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo ">> Copying wp-config.php into mounted /var/www/html..."
-cp /tmp/wp-config.php /var/www/html/wp-config.php
+cp /tmp_wp-config.php /var/www/html/wp-config.php
 
 # Then exec the original command (starts Apache)
 /usr/local/bin/docker-entrypoint.sh apache2-foreground
