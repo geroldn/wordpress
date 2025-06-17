021992dc234353c4586a32b5496205271de0f095
geroldn <gerold@nicolasen.com>
2025-06-17 21:42:27 +0200

    changed entrypoint to copy wp-config.php

---
+++ pl_entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo ">> Copying wp-config.php into mounted /var/www/html..."
+cp /tmp/wp-config.php /var/www/html/wp-config.php
+
+# Then exec the original command (starts Apache)
+/usr/local/bin/docker-entrypoint.sh apache2-foreground
+
